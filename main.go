0351fbc2f009a9568d1555fdbddf300f2354760e
Mischa Jörg <joergmis@students.zhaw.ch>
2019-03-09 00:30:28 +0100

    add login

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -11,6 +12,16 @@ const (
 	port = ":8000"
 )
 
+type person struct {
+	PW   string `json:"pw"`
+	Name string `json:"name"`
+}
+
+type rental struct {
+	Duration string `json:"duration"`
+	Name     string `json:"name"`
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", checkLogin).Methods("GET")
@@ -20,9 +31,17 @@ func main() {
 }
 
 func checkLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world!"))
+	var p person
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil || p.PW != "test" {
+		log.Fatalf("error: %e or wrong password", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("user %s now logged in", p.Name)
+	w.WriteHeader(http.StatusOK)
 }
 
 func rentCar(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world!"))
+	json.NewEncoder(w).Encode("hello world")
 }
